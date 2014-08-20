bce857ec223fd54dcac014b6b97e6b93a3a99908
jonny <jonny@pinae.net>
2014-08-21 01:58:11 +0900

    Refactored create_height_map

--- Heightmap.py
+++ Heightmap.py
@@ -4,7 +4,7 @@
 import pygame
 import sys
 import numpy as np
-from random import randint, shuffle, random
+from random import randint, shuffle
 from Config import mapdimensions, slope, slopediag, directions
 
 
@@ -66,62 +66,26 @@ def create_height_map(water_map, screen):
     for x in water_map:
         for y in x:
             y[0] /= 30
-    fails = 0
-    update = 0
-    start_phase = 400
-    while fails < 10:
-        pix = (randint(0, mapdimensions[0] - 1), randint(0, mapdimensions[1] - 1))
-        if height_map[pix[0], pix[1], 0] == height_map[pix[0], pix[1], 1]:
-            fails += 1
-            #print("Fails erhöht auf "+str(fails))
-        else:
-            fails = 0
-            #print("New Pixel ____________________________________________")
-            #raw_input("Press Enter to continue...")
-            #print("Fails auf 0 gesetzt")
-            min_value = height_map[pix[0], pix[1], 0]
-            max_value = height_map[pix[0], pix[1], 1]
-            #height = randint(min_value, max_value)
-            rnd = random()
-            height = min_value + int(round(
-                (5 * rnd * rnd * rnd * rnd + rnd * rnd * rnd + rnd * rnd + 5 * rnd) / 12.0 * (max_value - min_value)
-            ))
-            height_map[pix[0], pix[1], 2] = height
-            #min- und maxmap updaten
-            height_map[pix[0], pix[1], 0] = height
-            height_map[pix[0], pix[1], 1] = height
-            if max_value-min_value > 10:
-                update_min_map(height_map, water_map, pix, height)
-                update_max_map(height_map, water_map, pix, height)
-            else:
-                update_min_map(height_map, water_map, pix, height)
-                update_max_map(height_map, water_map, pix, height)
-            #Anzeige
-            if update == 0:
-                if start_phase > 0:
-                    update = 1
-                else:
-                    update = 75 + 10 * (fails + 1) * (fails + 1)
-                pygame.surfarray.blit_array(screen, height_map)
-                pygame.display.flip()
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-            update -= 1
-            start_phase -= 1
-    print("Zufälliges setzen der Pixel beendet. Jetzt systematisch.")
-    for x in range(len(height_map)):
-        for y in range(len(height_map[x])):
-            height = randint(height_map[x, y, 0], height_map[x, y, 1])
-            height_map[x, y, 2] = height
-            #min- und maxmap updaten
-            height_map[x, y, 0] = height
-            height_map[x, y, 1] = height
-            update_min_map(height_map, water_map, (x, y), height)
-            update_max_map(height_map, water_map, (x, y), height)
-        pygame.surfarray.blit_array(screen, height_map)
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    all_points = []
+    for x in range(mapdimensions[0]):
+        for y in range(mapdimensions[1]):
+            all_points.append((x, y))
+    shuffle(all_points)
+    for pix_no, pix in enumerate(all_points):
+        min_value = height_map[pix[0], pix[1], 0]
+        max_value = height_map[pix[0], pix[1], 1]
+        height = randint(min_value, max_value)
+        height_map[pix[0], pix[1], 2] = height
+        # update min- and maxmap
+        height_map[pix[0], pix[1], 0] = height
+        height_map[pix[0], pix[1], 1] = height
+        update_min_map(height_map, water_map, pix, height)
+        update_max_map(height_map, water_map, pix, height)
+        # view update
+        if pix_no < mapdimensions[0] + mapdimensions[1] or pix[0] == 0:
+            pygame.surfarray.blit_array(screen, height_map)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
     return height_map

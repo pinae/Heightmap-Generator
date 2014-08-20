50d8151dc16d7c93056306e87ad64a12188ab7e0
jonny <jonny@pinae.net>
2014-08-21 02:02:43 +0900

    Added finished signal

--- Heightmap-Generator.py
+++ Heightmap-Generator.py
@@ -3,7 +3,6 @@
 
 import pygame
 import sys
-import numpy as np
 
 from Config import mapdimensions
 from Watermap import create_water_map
@@ -19,7 +18,6 @@ def main():
     # create the water_map and display it
     flow_points = [((0, 50), 0)]
     water_map = create_water_map(screen, flow_points)
-    #water_map = np.array([np.array([np.array([255, 0, 0]) for y in range(mapdimensions[1])]) for x in range(mapdimensions[0])])
     pygame.surfarray.blit_array(screen, water_map)
     pygame.display.flip()
     print("water map created.")
@@ -31,6 +29,7 @@ def main():
     pygame.surfarray.blit_array(screen, height_map)
     pygame.display.flip()
     # shutdown if requested
+    print("Finished.")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
